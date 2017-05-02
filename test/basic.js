29c35bb0b46eb774f6cb7c53d3980ced8720369c
Batiste Bieler <batiste.bieler@gmail.com>
2017-05-02 14:31:28 +0200

    Fix the test

--- test/basic.js
+++ test/basic.js
@@ -7,8 +7,7 @@ describe('Compiler', function () {
   it('should return -1 when the value is not present', function () {
     var ast = vDom.ast('tpl/all.pug', './tpl')
     var compiler = new vDom.Compiler(ast)
-    compiler.compile()
-    var code = compiler.buffer.join('')
+    var code = compiler.compile()
     assert.ok(code)
     var render = new vm.Script(code + '\r\nrender({}, function(){})')
     var context = new vm.createContext({variable: 1, msg: 'Blop', friends: []})
