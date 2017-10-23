a5b55ac76b22d613782b994377c6231f02f5f4f5
Gryphon Myers <gryphon.myers@2k.com>
2017-10-23 11:30:59 -0700

    runtime cleanup

--- runtime.js
+++ runtime.js
@@ -3,10 +3,6 @@ var mergeAttrs = {class: 1};
 exports.compileAttrs = compileAttrs;
 global.pugVDOMRuntime = exports
 
-function stripQuotes(str) {
-    return str.replace(/"|'/g,"");
-}
-
 function compileAttrs(attrs, attrBlocks) {
     var attrsObj = attrBlocks
         .reduce(function(finalObj, currObj) {
@@ -15,7 +11,7 @@ function compileAttrs(attrs, attrBlocks) {
             }
             return finalObj;
         }, attrs.reduce(function(finalObj, attr) {
-            var val = attr.val;//typeof attrstripQuotes(attr.val);
+            var val = attr.val;
             finalObj[attr.name] = finalObj[attr.name] ? finalObj[attr.name].concat(val) : [val]
             return finalObj;
         }, {}));
