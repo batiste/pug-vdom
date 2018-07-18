c948cf54f945a0009553e3351137ba3befbc012a
Batiste Bieler <batiste.bieler@gmail.com>
2018-07-18 09:23:47 +0200

    Update runtime

--- dist/runtime.js
+++ dist/runtime.js
@@ -1,9 +1,18 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.pugVDOMRuntime = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-var mergeAttrs = {class: 1};
-
 exports.compileAttrs = compileAttrs;
+exports.exposeLocals = exposeLocals;
+exports.deleteExposedLocals = deleteExposedLocals;
+
 global.pugVDOMRuntime = exports
+
+if (!global) global = window;
+
+var flatten = function(arr) {
+    return Array.prototype.concat.apply([], arr); 
+};
+
+var exposedLocals = {};
 
 function compileAttrs(attrs, attrBlocks) {
     var attrsObj = attrBlocks
@@ -14,17 +23,55 @@ function compileAttrs(attrs, attrBlocks) {
             return finalObj;
         }, attrs.reduce(function(finalObj, attr) {
             var val = attr.val;
-            finalObj[attr.name] = finalObj[attr.name] ? finalObj[attr.name].concat(val) : [val]
+            finalObj[attr.name] = finalObj[attr.name] ? finalObj[attr.name].concat(val) : [val];
             return finalObj;
         }, {}));
-
+    
     for (var propName in attrsObj) {
-        attrsObj[propName] = mergeAttrs[propName] ? attrsObj[propName].join(' ') : attrsObj[propName].pop();
+        if (propName === 'class') {
+            attrsObj[propName] = flatten(attrsObj[propName].map(attrValue => {
+                if (attrValue && typeof attrValue === 'object' && !Array.isArray(attrValue)) {
+                    var classResult = [];
+                    for (var className in attrValue) {
+                        if (attrValue[className]) {
+                            classResult.push(className);
+                        }
+                    }
+                    return classResult;
+                }
+                return attrValue;
+            })).join(' ');            
+        } else {
+            attrsObj[propName] = attrsObj[propName].pop();
+        }
     }
 
     return attrsObj;
 }
 
+function exposeLocals(locals) {
+    return Object.keys(locals).reduce(function(acc, prop) {
+        if (!(prop in global))  {
+            Object.defineProperty(global, prop, {
+                configurable: true, 
+                get: function() {
+                    return locals[prop]
+                }
+            });
+            exposedLocals[prop] = 1;
+        } else {
+            acc[prop] = 1;
+        }
+        return acc
+    }, {})
+}
+
+function deleteExposedLocals() {
+    for (var prop in exposedLocals) {
+        delete global[prop];
+        delete exposedLocals[prop];
+    }
+}
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
 });
